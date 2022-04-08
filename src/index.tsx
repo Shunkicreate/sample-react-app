d9daed8cb5bb736fff880018cecfc4b66d8a17fc
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2022-04-08 14:09:56 +0900

    second

--- src/index.tsx
+++ src/index.tsx
@@ -18,24 +18,79 @@ import reportWebVitals from './reportWebVitals';
 //   {className: 'greeting'},
 //   'Hello, world    !'
 // )
-interface propType{
-  name:string
+interface propType {
+  welcome: {
+    name: string
+  }
+  clock: {
+    date: Date
+  }
+  defclass:{
+    date:Date
+  }
 }
-function Welcome(props:propType){
+function Welcome(props: propType["welcome"]) {
   return <h1>Hello, {props.name}</h1>
 }
 
-function tick(){
-  const element = (
-    <div>
-      <Welcome name="haha"></Welcome>
-      <h1>hello world</h1>
-      <h2>It is {new Date().toLocaleTimeString()}</h2>
-    </div>
-  );
-  ReactDOM.render(element, document.getElementById('root'));
+interface Props {
+  date: Date;
+  timerID: number | undefined
 }
-setInterval(tick, 1000);
+interface State {
+  date: Date;
+  // timerID: number | undefined
+}
+
+class Clock extends React.Component<Props, State> {
+  constructor(props:Props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  componentDidMount() {
+    const timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(timerID);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
+
+// function Clock(props: propType["clock"]) {
+//   return (
+//     <div>
+//       <Welcome name="hahaha" ></Welcome>
+//       <h1>Hello, world!</h1>
+//       <h2>It is {props.date.toLocaleTimeString()}.</h2>
+//     </div>
+//   );
+// }
+
+// function tick() {
+//   ReactDOM.render(
+//     <Clock date={new Date()} />,
+//     document.getElementById('root')
+//   );
+// }
+ReactDOM.render(
+  <Clock />,
+  document.getElementById('root')
+);
+// setInterval(tick, 1000);
+
 
 
 // ReactDOM.render(
